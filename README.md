7d3a16b52b8824f609d30cca49db4a8dcf892dc6
Frank Wang <yf.frank.wang@outlook.com>
2022-04-08 21:31:10 +0900

    add jquery.ui.toast.js

--- README.md
+++ README.md
@@ -24,45 +24,19 @@ Features
    | jquery.disable.js            | $.disable function                  |
    | jquery.jscript.js            | dynamic load javascript             |
    | jquery.ui.center.js          | Move the selected element(s) to the center position of parent element (default: body)               |
-   | jquery.ui.editable-select.js | a simple plugin that converts a select into an text field with suggestions |
    | jquery.ui.fieldset.js        | collapseable fieldset               |
    | jquery.ui.loadmask.js        | loader mask                         |
    | jquery.ui.popup.js           | a popup pulldown                    |
    | jquery.ui.scrollIntoView.js  | $.scrollIntoView function           |
    | jquery.ui.selecttext.js      | $.selectText function               |
    | jquery.ui.textarea.js        | textarea auto expand and fire event on Ctrl+Enter   |
+   | jquery.ui.toast.js           | notification (toast) plugin         |
 
  - jQuery Extra
 
    | Name                     | Description                              |
    |--------------------------|------------------------------------------|
-   | jquery.ui.hammer.js      | Gestures for TouchPad device             |
    | jquery.ui.lightbox.js    | LightBox Plugin                          |
-   | jquery.ui.meio.mask.js   | A jquery plugin for masking text inputs  |
-   | jquery.ui.mousewheel.js  | A jquery plugin for mouse wheel support  |
-   | jquery.ui.notify.js      | notification plugin                      |
-   | jquery.ui.tablesorter.js | A jquery plugin for html table sort      |
-
- - Panda
-
-   | Name                     | Description                                                              |
-   |--------------------------|--------------------------------------------------------------------------|
-   | core.array.js            | functions for Array                                                      |
-   | core.date.js             | data formatter parser                                                    |
-   | core.function.js         | functions for Function                                                   |
-   | core.json.js             | JSON parser                                                              |
-   | core.number.js           | parser, formatter, trim for Number or number string                      |
-   | core.string.js           | functions for String                                                     |
-   | ui.alert.js              | $.palert for panda.mvc.view.tag.ui.ActionMessage                         |
-   | ui.checkboxlist.js       | a function for ordered panda.mvc.view.tag.ui.CheckboxList                |
-   | ui.forms.js              | functions(hook/focus/fire) for Form tag                                  |
-   | ui.listview.js           | functions for panda.mvc.view.tag.ui.ListView                             |
-   | ui.pager.js              | functions for panda.mvc.view.tag.ui.Pager                                |
-   | ui.panel.js              | mousewheel horizental scrollable panel (require jquery.ui.mousewheel.js) |
-   | ui.site.js               | functions for site                                                       |
-   | ui.trigger.js            | functions for panda.mvc.view.tag.ui.TriggerField                         |
-   | ui.uploader.js           | functions for panda.mvc.view.tag.ui.Uploader                             |
-   | ui.viewfield.js          | functions for panda.mvc.view.tag.ui.ViewField                            |
 
 
 
@@ -78,16 +52,8 @@ Panda HTML use the following software packages:
 | Chart.js                | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/chartjs/Chart.js](https://github.com/chartjs/Chart.js) |
 | flag-icon-css           | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/lipis/flag-icon-css](https://github.com/lipis/flag-icon-css) |
 | Font Awesome            | [Font Awesome](http://fontawesome.io/license/)                | [http://fontawesome.io/](http://fontawesome.io/) |
-| Hammer.js               | [MIT](https://opensource.org/licenses/MIT)                    | [http://hammerjs.github.io/](http://hammerjs.github.io/) |
 | jQuery                  | [jQuery](https://jquery.org/license/)                         | [https://jquery.com/](https://jquery.com/) |
-| jquery-cleditor         | [MIT](https://opensource.org/licenses/MIT)                    | [http://premiumsoftware.net/cleditor/](http://premiumsoftware.net/cleditor/) |
-| jquery-editable-select  | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/indrimuska/jquery-editable-select](https://github.com/indrimuska/jquery-editable-select) |
 | jquery-lightbox         |                                                               | [http://leandrovieira.com](http://leandrovieira.com) |
-| jquery-meiomask         | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/fabiomcosta/jquery-meiomask](https://github.com/fabiomcosta/jquery-meiomask) |
-| jquery-mousewheel       | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/jquery/jquery-mousewheel](https://github.com/jquery/jquery-mousewheel) |
-| Notify.js               | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/jpillora/notifyjs](https://github.com/jpillora/notifyjs) |
-| Respond.js              | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/scottjehl/Respond](https://github.com/scottjehl/Respond) |
+| jquery-toast            | [MIT](https://opensource.org/licenses/MIT)                    | [https://github.com/kamranahmedse/jquery-toast-plugin](https://github.com/kamranahmedse/jquery-toast-plugin) |
 | summernote              | [MIT](https://opensource.org/licenses/MIT)                    | [https://summernote.org](https://summernote.org) |
-| tablesorter             | [MIT](https://opensource.org/licenses/MIT)                    | [http://tablesorter.com](http://tablesorter.com) |
-| TinyMCE                 | [LGPL](http://www.gnu.org/licenses/lgpl.html)                 | [https://github.com/tinymce/tinymce](https://github.com/tinymce/tinymce) |
 
--- html/jquery/js/jquery-plugins.js
+++ html/jquery/js/jquery-plugins.js
@@ -1415,6 +1415,323 @@ jQuery.jcookie = function(name, value, options) {
 		$('textarea[autosize]').autosize();
 	});
 })(jQuery);
+// jQuery toast plugin created by Kamran Ahmed copyright MIT license 2015 (modified by Frank Wang)
+(function($) {
+	function Toast(options) {
+		var os = {}, // options
+			$t; // toast-single
+
+		function prepareOptions(options, options_to_extend) {
+			var e = options_to_extend || {},
+				o = {};
+
+			if ((typeof options === 'string') || $.isArray(options)) {
+				o.text = options;
+			} else {
+				o = options;
+			}
+			$.extend(os, e, o);
+		}
+
+		function setup() {
+			$t = $t || $('<div class="ui-toast-single"></div>');
+
+			$t.empty();
+
+			// For the loader on top
+			$t.append($('<span class="ui-toast-loader"></span>'));
+
+			if (os.closeable) {
+				$t.append($('<span class="ui-toast-close">&times;</span>'));
+			}
+
+			var sm = os.html ? 'html' : 'text';
+			if (os.heading) {
+				$t.append($('<h2 class="ui-toast-heading">')[sm](os.heading));
+			}
+
+			if ($.isArray(os.text)) {
+				var $ul = $('<ul class="ui-toast-ul">');
+				for (var i = 0; i < os.text.length; i++) {
+					$ul.append($('<li class="ui-toast-li" id="ui-toast-item-' + i + '">')[sm](os.text[i]));
+				}
+				$t.append($ul);
+			} else {
+				$t.append($('<span>')[sm](os.text));
+			}
+
+			if (os.bgColor !== false) {
+				$t.css("background-color", os.bgColor);
+			};
+
+			if (os.textColor !== false) {
+				$t.css("color", os.textColor);
+			};
+
+			if (os.textAlign) {
+				$t.css('text-align', os.textAlign);
+			}
+
+			if (os.icon !== false) {
+				$t.addClass('ui-toast-has-icon ui-toast-icon-' + os.icon);
+			}
+
+			if (os['class'] !== false) {
+				$t.addClass(os['class'])
+			}
+		}
+
+		function position() {
+			var $c = $(".ui-toast-wrap"),
+				sp = os.position,
+				op = {
+					left: 'auto',
+					top: 'auto',
+					right: 'auto',
+					bottom: 'auto'
+				};
+			
+			if (typeof sp === 'object') {
+				$.extend(op, sp);
+			} else {
+				switch (sp) {
+				case 'mid-center':
+					op.left = ($(window).outerWidth() / 2) - $c.outerWidth() / 2;
+					op.top = ($(window).outerHeight() / 2) - $c.outerHeight() / 2;
+					break;
+				case 'bottom':
+					op.bottom = 5;
+					op.left = 20;
+					op.right = 20;
+					break;
+				case 'bottom-center':
+					op.left = ($(window).outerWidth() / 2) - $c.outerWidth() / 2;
+					op.bottom = 20;
+					break;
+				case 'bottom-left':
+					op.bottom = 20;
+					op.left = 20;
+					break;
+				case 'bottom-right':
+					op.bottom = 20;
+					op.right = 20;
+					break;	
+				case 'top':
+					op.top = 5;
+					op.left = 20;
+					op.right = 20;
+					break;
+				case 'top-center':
+					op.left = ($(window).outerWidth() / 2) - $c.outerWidth() / 2;
+					op.top = 20;
+					break;
+				case 'top-left':
+					op.top = 20;
+					op.left = 20;
+					break;
+				case 'top-right':
+				default:
+					op.top = 20;
+					op.right = 20;
+					break;
+				}
+			}
+			$c.css(op);
+		}
+
+		function bindToast() {
+			$t.on('afterShown', function() {
+				processLoader();
+			});
+
+			$t.find('.ui-toast-close').on('click', function(e) {
+				e.preventDefault();
+				transitionOut();
+			});
+
+			if (typeof os.beforeShow == 'function') {
+				$t.on('beforeShow', function() {
+					os.beforeShow($t);
+				});
+			}
+
+			if (typeof os.afterShown == 'function') {
+				$t.on('afterShown', function() {
+					os.afterShown($t);
+				});
+			}
+
+			if (typeof os.beforeHide == 'function') {
+				$t.on('beforeHide', function() {
+					os.beforeHide($t);
+				});
+			}
+
+			if (typeof os.afterHidden == 'function') {
+				$t.on('afterHidden', function() {
+					os.afterHidden($t);
+				});
+			}
+
+			if (typeof os.onClick == 'function') {
+				$t.on('click', function() {
+					os.onClick($t);
+				});
+			}
+		}
+
+		function addToDom() {
+			var $c = $('.ui-toast-wrap'),
+				sn = os.stack;
+
+			if ($c.length === 0) {
+				$c = $('<div></div>', {
+					"class": "ui-toast-wrap",
+					"role": "alert",
+					"aria-live": "polite"
+				});
+
+				$('body').append($c);
+
+			} else if (!sn || isNaN(parseInt(sn, 10))) {
+				$c.empty();
+			}
+
+			$c.find('.ui-toast-single:hidden').remove();
+
+			$c.append($t);
+
+			if (sn && !isNaN(parseInt(sn), 10)) {
+				var _prevToastCount = $c.find('.ui-toast-single').length,
+					_nextToastCount = _prevToastCount - sn;
+
+				if (_nextToastCount > 0) {
+					$c.find('.ui-toast-single').slice(0, _nextToastCount).remove();
+				}
+			}
+		}
+
+		function canAutoHide() {
+			return (os.hideAfter !== false) && !isNaN(parseInt(os.hideAfter, 10));
+		}
+
+		function processLoader() {
+			// Show the loader only, if auto-hide is on and loader is demanded
+			if (!canAutoHide() || os.loader === false) {
+				return false;
+			}
+
+			var loader = $t.find('.ui-toast-loader');
+
+			// 400 is the default time that jquery uses for fade/slide
+			// Divide by 1000 for milliseconds to seconds conversion
+			var transitionTime = (os.hideAfter - 400) / 1000 + 's';
+			var loaderBg = os.loaderBg;
+
+			var style = loader.attr('style') || '';
+			style = style.substring(0, style.indexOf('-webkit-transition')); // Remove the last transition definition
+
+			style += '-webkit-transition: width ' + transitionTime + ' ease-in; \
+					  -o-transition: width ' + transitionTime + ' ease-in; \
+					  transition: width ' + transitionTime + ' ease-in; \
+					  background-color: ' + loaderBg + ';';
+
+			loader.attr('style', style).addClass('ui-toast-loaded');
+		}
+
+		function animate() {
+			transitionIn();
+
+			if (canAutoHide()) {
+				setTimeout(function() {
+					transitionOut();
+				}, os.hideAfter);
+			}
+		}
+
+		function transitionIn() {
+			var tm = 'show';
+
+			switch (os.transition) {
+			case 'fade':
+				tm = 'fadeIn';
+				break;
+			case 'slide':
+				tm = 'slideDown';
+				break;
+			}
+
+			$t.hide().trigger('beforeShow')[tm](function() {
+				$t.trigger('afterShown');
+			});
+		}
+
+		function transitionOut() {
+			var tm = 'hide';
+
+			switch (os.transition) {
+			case 'fade':
+				tm = 'fadeOut';
+				break;
+			case 'slide':
+				tm = 'slideUp';
+				break;
+			}
+
+			$t.trigger('beforeHide')[tm](function() {
+				$t.trigger('afterHidden');
+			});
+		}
+
+		prepareOptions(options, $.toast.defaults);
+		setup();
+		addToDom();
+		position();
+		bindToast();
+		animate();
+
+		var api = {
+			reset: function(resetWhat) {
+				if (resetWhat === 'all') {
+					$('.ui-toast-wrap').remove();
+				} else {
+					$t.remove();
+				}
+			},
+	
+			update: function(options) {
+				prepareOptions(options);
+				setup();
+				bindToast();
+			},
+	
+			clsose: function() {
+				transitionOut();
+			}
+		};
+
+		return api;
+	}
+
+	$.toast = Toast;
+
+	$.toast.defaults = {
+		icon: false,
+		text: '',
+		heading: '',
+		loader: true,
+		transition: 'fade',
+		closeable: true,
+		hideAfter: 5000,
+		stack: 5,
+		position: 'top-right',
+		bgColor: false,
+		textColor: false,
+		textAlign: 'left',
+		loaderBg: '#9EC600'
+	};
+
+})(jQuery);
 (function($) {
 	$.fn.totop = function() {
 		$(this).each(function() {
