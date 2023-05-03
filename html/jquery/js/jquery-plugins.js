841a6123b2b694534d8fa6734ea1a718dbb87b42
Frank Wang <yf.frank.wang@outlook.com>
2023-05-03 21:29:14 +0900

    nice-select: do update() if niceSelect is created

--- html/jquery/js/jquery-plugins.js
+++ html/jquery/js/jquery-plugins.js
@@ -1401,31 +1401,29 @@
 	}
 
 	function create_nice_select($select) {
-		$select.after($('<div></div>')
-			.addClass('ui-nice-select')
-			.addClass($select.attr('class') || '')
-			.addClass($select.attr('disabled') ? 'disabled' : '')
-			.attr('tabindex', $select.attr('disabled') ? null : '0')
-			.html('<span class="current"></span><ul></ul>')
-		);
+		var $options = $select.find('option'),
+			$selected = $select.find('option:selected'),
+			$current = $('<span class="current"></span>'),
+			$ul = $('<ul></ul>'),
+			$dropdown = $('<div></div>')
+				.addClass('ui-nice-select')
+				.addClass($select.attr('class') || '')
+				.addClass($select.attr('disabled') ? 'disabled' : '')
+				.attr('tabindex', $select.attr('disabled') ? null : ($select.attr('tabindex') || '0'))
+				.append($current, $ul);
 
-		var $dropdown = $select.next();
-		var $options = $select.find('option');
-		var $selected = $select.find('option:selected');
-
-		$dropdown.find('.current').html($selected.data('display') || $selected.text());
+		$current.text($selected.data('display') || $selected.text());
 
 		$options.each(function() {
 			var $option = $(this);
-			var display = $option.data('display');
 
-			$dropdown.find('ul').append($('<li></li>')
+			$ul.append($('<li></li>')
 				.attr('data-value', $option.val())
-				.attr('data-display', (display || null))
+				.attr('data-display', ($option.data('display') || null))
 				.addClass(
 					($option.is(':selected') ? ' selected' : '') +
 					($option.is(':disabled') ? ' disabled' : ''))
-				.html($option.text())
+				.text($option.text())
 			);
 		});
 
@@ -1437,6 +1435,8 @@
 
 		// Option click
 		$dropdown.on('click', 'li:not(.disabled)', __dropdown_option_click);
+
+		$select.after($dropdown);
 	}
 
 	var api = {
@@ -1456,10 +1456,11 @@
 
 		// Create custom markup
 		this.each(function() {
-			var $select = $(this);
-
-			if (!$select.next().hasClass('ui-nice-select')) {
-				create_nice_select($select);
+			var $s = $(this);
+			if ($s.next().hasClass('ui-nice-select')) {
+				update.apply($s);
+			} else {
+				create_nice_select($s);
 			}
 		});
 
