1879d6072104ca69ab0390dca3152220d35d9c25
Frank Wang <yf.frank.wang@outlook.com>
2023-07-22 13:15:54 +0900

    jquery: add ui.form-action.js

--- html/jquery/js/jquery-plugins.js
+++ html/jquery/js/jquery-plugins.js
@@ -747,6 +747,17 @@
 (function($) {
 	"use strict";
 
+	$(window).on('load', function() {
+		$('input[data-action], button[data-action]').off('click.action').on('click.action', function() {
+			$(this).closest('form').attr('action', $(this).data('action'));
+		});
+	});
+
+})(jQuery);
+
+(function($) {
+	"use strict";
+
 	$.fn.changeValue = function(v) {
 		var o = this.val();
 
