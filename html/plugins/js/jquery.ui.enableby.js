eb8b2fee7296f4527eb9e989da44f9b70d165277
Frank Wang <yf.frank.wang@outlook.com>
2024-03-14 23:50:41 +0900

    ui: checkall, enableby: use $.on(e, filter, ...) for target element removed or inserted

--- html/plugins/js/jquery.ui.enableby.js
+++ html/plugins/js/jquery.ui.enableby.js
@@ -1,13 +1,24 @@
 (function($) {
 	"use strict";
 
-	$.fn.enableby = function(target) {
+	$.fn.enableby = function(s) {
 		$(this).each(function() {
-			var $a = $(this), $cs = $(target || $a.attr('enableby'));
-			if ($cs.length) {
-				$cs.change(function() {
-					$a.prop('disabled', $cs.filter(':checked').length == 0);
-				}).trigger('change');
+			var $a = $(this),
+				b = s || $a.attr('enableby'),
+				t = b, f = '',
+				i = b.indexOf(' ');
+
+			if (i > 0) {
+				t = b.substring(0, i);
+				f = b.substring(i+1);
+			}
+
+			if ($(b).length) {
+				$(t).on('change', f, function() {
+					$a.prop('disabled', $(b).filter(':checked').length == 0);
+				});
+				
+				$(b).first().trigger('change');
 			} else {
 				$a.prop('disabled', true);
 			}
