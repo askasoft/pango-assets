67c3145ad1a9a99ab64bbb258eff0bdcf7ab4de5
Frank Wang <yf.frank.wang@outlook.com>
2024-05-13 20:23:21 +0900

    ui: add jquery.ui.checkorder.js

--- html/plugins/js/plugins.js
+++ html/plugins/js/plugins.js
@@ -696,6 +696,36 @@
 (function($) {
 	"use strict";
 
+	var E = 'click.checkorder';
+
+	function _click() {
+		var $c = $(this), $l = $c.closest('label'), $h = $c.closest('.ui-checks').find('hr');
+		$l.fadeOut(200, function() {
+			if ($c.is(':checked')) {
+				$l.insertBefore($h);
+			} else {
+				$l.insertAfter($h);
+			}
+			$l.fadeIn(200);
+		});
+	}
+
+	$.fn.checkorder = function() {
+		var $t = $(this), $h = $t.find('hr');
+		if ($h.length == 0) {
+			$t.prepend($('<hr>'));
+		}
+		$t.find(":checkbox").off(E).on(E, _click);
+	}
+
+	// ==================
+	$(window).on('load', function() {
+		$('.ui-checks.ordered').checkorder();
+	});
+})(jQuery);
+(function($) {
+	"use strict";
+
 	$.fn.enableby = function(s) {
 		$(this).each(function() {
 			var $a = $(this),
