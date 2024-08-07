58bc5ec465b9bba402043efa09aceb12c727f901
Frank Wang <yf.frank.wang@outlook.com>
2024-08-08 01:51:02 +0900

    corejs/function: fix delegate(), delay() and scope

--- html/corejs/core.function.js
+++ html/corejs/core.function.js
@@ -1,13 +1,32 @@
 (function() {
 	"use strict";
 
+	if (typeof Function.prototype.bind != "function") {
+		/**
+		 * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+		 * 
+		 * Syntax:
+		 * <pre><code>
+			fun.bind(thisArg[, arg1[, arg2[, ...]]])
+			</code></pre>
+		 * @return {Function} The new function
+		 */
+		Function.prototype.bind = function(scope/*, args...*/) {
+			// make args available, in function below
+			var fn = this, args = [].slice.call(arguments, 1);
+			return function() {
+				return fn.apply(scope, args);
+			};
+		};
+	}
+
 	if (typeof Function.prototype.callback != "function") {
 		/**
 		 * Creates a callback that passes arguments[0], arguments[1], arguments[2], ...
 		 * Call directly on any function. Example: <code>myFunction.callback(arg1, arg2)</code>
 		 * Will create a function that is bound to those 2 args. <b>If a specific scope is required in the
 		 * callback, use {@link #delegate} instead.</b> The function returned by callback always
-		 * executes in the this or window scope.
+		 * executes in the caller scope.
 		 * <p>This method is required when you want to pass arguments to a callback function.  If no arguments
 		 * are needed, you can simply pass a reference to the function as a callback (e.g., callback: myFn).
 		 * However, if you tried to pass a function with arguments (e.g., callback: myFn(arg1, arg2)) the function
@@ -26,31 +45,9 @@
 		 */
 		Function.prototype.callback = function(/*args...*/) {
 			// make args available, in function below
-			var method = this;
-			var args = [].slice.call(arguments, 0);
+			var fn = this, args = [].slice.call(arguments, 0);
 			return function() {
-				return method.apply(this || window, args.concat([].slice.call(arguments, 0)));
-			};
-		};
-	}
-
-	if (typeof Function.prototype.bind != "function") {
-		/**
-		 * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
-		 * 
-		 * Syntax:
-		 * <pre><code>
-			fun.bind(thisArg[, arg1[, arg2[, ...]]])
-			</code></pre>
-		 * @return {Function} The new function
-		 */
-		Function.prototype.bind = function(/*args...*/) {
-			// make args available, in function below
-			var method = this;
-			var scope = arguments[0] || window;
-			var args = [].slice.call(arguments, 1);
-			return function() {
-				return method.apply(scope, args);
+				return fn.apply(this, args.concat([].slice.call(arguments, 0)));
 			};
 		};
 	}
@@ -58,9 +55,9 @@
 	if (typeof Function.prototype.delegate != "function") {
 		/**
 		 * Creates a delegate (callback) that sets the scope to arguments[0].
-		 * Call directly on any function. Example: <code>this.myFunction.delegate(this, arg1, arg2)</code>
-		 * Will create a function that is automatically scoped to obj so that the <tt>this</tt> variable inside the
-		 * callback points to obj. Example usage:
+		 * Call directly on any function. Example: <code>this.myFunction.delegate(this, [ arg1, arg2 ])</code>
+		 * Will create a function that is automatically scoped to scope so that the <tt>this</tt> variable inside the
+		 * callback points to scope. Example usage:
 		 * <pre><code>
 			var sayHi = function(name, event) {
 				// Note this use of "this.text()" here.
@@ -74,16 +71,20 @@
 			// This callback will execute in the scope of the
 			// button instance. Clicking the button alerts
 			// "Hi, Fred. You clicked the "Say Hi" button."
-			btn.on('click', sayHi.delegate(btn, 'Fred'));
+			btn.on('click', sayHi.delegate(btn, [ 'Fred' ]));
 			</code></pre>
+		 * @param {Object} scope (optional) The object for which the scope is set
+		 * @param {Array} args (optional) Overrides arguments for the call. (Defaults to the arguments passed by the caller)
+		 * @param {Boolean} append (optional) if True args are appended to the call arguments instead of prepending
 		 * @return {Function} The new function
 		 */
-		Function.prototype.delegate = function(/*args...*/) {
-			var method = this;
-			var scope = arguments[0] || window;
-			var args = [].slice.call(arguments, 1);
+		Function.prototype.delegate = function(scope, args, append) {
+			var fn = this;
 			return function() {
-				return method.apply(scope, args.concat([].slice.call(arguments, 0)));
+				var g = [].slice.call(arguments, 0);
+				args ||= [];
+				args = append ? g.concat(args) : args.concat(g);
+				return fn.apply(scope || this, args);
 			};
 		};
 	}
@@ -92,7 +93,7 @@
 		/**
 		 * Calls this function after the number of millseconds specified, optionally in a specific scope. Example usage:
 		 * <pre><code>
-			var sayHi = function(name){
+			var sayHi = function(name) {
 				alert('Hi, ' + name);
 			}
 	
@@ -100,21 +101,16 @@
 			sayHi('Fred');
 	
 			// executes after 2 seconds:
-			sayHi.delay(2000, this, ['Fred']);
+			sayHi.delay(2000, this, 'Fred');
 	
-			// this syntax is sometimes useful for deferring
-			// execution of an anonymous function:
-			(function(){
-				alert('Anonymous');
-			}).delay(100);
 			</code></pre>
 		 * @param {Number} millis The number of milliseconds for the setTimeout call (if 0 the function is executed immediately)
-		 * @param {Object} obj (optional) The object for which the scope is set
-		 * @param {Array} args (optional) Overrides arguments for the call. (Defaults to the arguments passed by the caller)
+		 * @param {Object} scope (optional) The object for which the scope is set
+		 * @param {...} args (optional) Arguments for the call.
 		 * @return {Number} The timeout id that can be used with clearTimeout
 		 */
-		Function.prototype.delay = function(millis, obj, args) {
-			var fn = this.delegate(obj, args);
+		Function.prototype.delay = function(millis/*, scope, args...*/) {
+			var fn = this.bind.apply([].slice.call(arguments, 1));
 			if (millis) {
 				return setTimeout(fn, millis);
 			}
@@ -129,7 +125,7 @@
 		 * the original one is not called. The resulting function returns the results of the original function.
 		 * The passed fcn is called with the parameters of the original function. Example usage:
 		 * <pre><code>
-			var sayHi = function(name){
+			var sayHi = function(name) {
 				alert('Hi, ' + name);
 			}
 	
@@ -137,30 +133,28 @@
 	
 			// create a new function that validates input without
 			// directly modifying the original function:
-			var sayHiToFriend = sayHi.precall(function(name){
+			var sayHiToFriend = sayHi.precall(function(name) {
 				return name == 'Brian';
 			});
 	
 			sayHiToFriend('Fred');	// no alert
 			sayHiToFriend('Brian'); // alerts "Hi, Brian"
 			</code></pre>
-		 * @param {Function} fcn The function to call before the original
+		 * @param {Function} pref The function to call before the original
 		 * @param {Object} scope (optional) The scope of the passed fcn (Defaults to scope of original function or window)
 		 * @return {Function} The new function
 		 */
-		Function.prototype.precall = function(fcn, scope) {
-			if (typeof fcn != "function") {
+		Function.prototype.precall = function(pref, scope) {
+			if (typeof pref != "function") {
 				return this;
 			}
 
-			var method = this;
+			var fn = this;
 			return function() {
-				fcn.target = this;
-				fcn.method = method;
-				if (fcn.apply(scope || this || window, arguments) === false) {
+				if (pref.apply(scope || this, arguments) === false) {
 					return;
 				}
-				return method.apply(this || window, arguments);
+				return fn.apply(this, arguments);
 			};
 		};
 	}
@@ -171,32 +165,32 @@
 		 * The resulting function returns the results of the original function.
 		 * The passed fcn is called with the parameters of the original function. Example usage:
 		 * <pre><code>
-			var sayHi = function(name){
+			var sayHi = function(name) {
 				alert('Hi, ' + name);
 			}
 	
 			sayHi('Fred'); // alerts "Hi, Fred"
 	
-			var sayGoodbye = sayHi.postcall(function(name){
+			var sayGoodbye = sayHi.postcall(function(name) {
 				alert('Bye, ' + name);
 			});
 	
 			sayGoodbye('Fred'); // both alerts show
 			</code></pre>
-		 * @param {Function} fcn The function to sequence
+		 * @param {Function} postf The function to sequence
 		 * @param {Object} scope (optional) The scope of the passed fcn (Defaults to scope of original function or window)
 		 * @return {Function} The new function
 		 */
-		Function.prototype.postcall = function(fcn, scope) {
-			if (typeof fcn != "function") {
+		Function.prototype.postcall = function(postf, scope) {
+			if (typeof postf != "function") {
 				return this;
 			}
 
-			var method = this;
+			var fn = this;
 			return function() {
-				var retval = method.apply(this || window, arguments);
-				fcn.apply(scope || this || window, arguments);
-				return retval;
+				var rv = fn.apply(this, arguments);
+				postf.apply(scope || this, arguments);
+				return rv;
 			};
 		};
 	}
